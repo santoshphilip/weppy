0a0538db9a6174f447bf4c5c99ce62cf46a1f420
Santosh Philip <you@example.com>
2016-10-26 11:13:27 -0700

    modified:   eppystuff.py     modified:   hello.py

--- eppystuff.py
+++ eppystuff.py
@@ -6,9 +6,23 @@ from eppy.modeleditor import IDF
 iddfhandle = StringIO.StringIO(iddcurrent.iddtxt)
 if IDF.getiddname() == None:
     IDF.setiddname(iddfhandle)
+idf = IDF(StringIO.StringIO(""))
+fname = '/Applications/EnergyPlus-8-3-0/ExampleFiles/5ZoneAirCooled.idf'
+idf = IDF(fname)
 
-def makeanidf():
+def getidf():
     """return an idf"""
-    fname = '/Applications/EnergyPlus-8-3-0/ExampleFiles/5ZoneAirCooled.idf'
-    idf = IDF(fname)
     return idf
+    
+def objnames():
+    """return obj names"""
+    return [anobj[0]['idfobj'] for anobj in idf.idd_info]   
+
+def page2():
+    idf = getidf()
+    onames = objnames()
+    n_len = [(objname, len(idf.idfobjects[objname.upper()])) for objname in onames]
+    nlen = [(name, ln)for name, ln in n_len if ln > 0]
+    return nlen
+
+
--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from bottle import route, run
 import eppystuff
+# idf = eppystuff.makeanidf()
 
 @route('/hello')
 def hello():
@@ -7,8 +8,22 @@ def hello():
 
 @route('/idf')
 def idf():
-    idf = eppystuff.makeanidf()
+    idf = eppystuff.getidf()
     lines = idf.idfstr().splitlines()
     return '<br>'.join(lines)
 
+@route('/page1')
+def page1():
+    objnames = eppystuff.objnames()
+    [objname for objname in objnames]
+    return '<br>'.join(objnames)
+
+@route('/page2')
+def page2():
+    nlens = eppystuff.page2()
+    nlens = ['%s -> %s' % (name, ln) for name, ln in nlens]
+    return '<br>'.join(nlens)
+    
+
 run(host='localhost', port=8080, debug=True)
+
